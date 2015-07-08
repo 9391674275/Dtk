f28cf2b6f92892eba717511ea57f0f45a1b6bfa5
prabhu <prabhu.1203@yahoo.com>
2015-07-08 18:18:09 +0530

    Dtk4

--- src/com/koya/MRA/DTK4.java
+++ src/com/koya/MRA/DTK4.java
@@ -3,6 +3,7 @@ package com.koya.MRA;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class DTK4 {
 
@@ -22,6 +23,60 @@ public class DTK4 {
 		driver.findElement(By.xpath(".//*[@id='sidebar']/ul/li[5]/ul/li[5]/a")).click();
 		driver.findElement(By.xpath(".//*[@id='sidebar']/ul/li[5]/ul/li[5]/ul/li[1]/a")).click();
 		Thread.sleep(5000);
+		new Select(driver.findElement(By.xpath(".//*[@id='ContentPlaceHolder1_ddlCounterParty']"))).selectByIndex(12);
+		Thread.sleep(2000);
+		new Select(driver.findElement(By.xpath(".//*[@id='ContentPlaceHolder1_ddlClientEntities']"))).selectByIndex(2);
+		Thread.sleep(3000);
+		
+		new Select(driver.findElement(By.xpath(".//*[@id='ContentPlaceHolder1_ddl_mm']"))).selectByIndex(2);
+		new Select(driver.findElement(By.xpath(".//*[@id='ContentPlaceHolder1_ddl_dd']"))).selectByIndex(2);
+		new Select(driver.findElement(By.xpath(".//*[@id='ContentPlaceHolder1_ddl_yyyy']"))).selectByIndex(2);
+		
+		new Select(driver.findElement(By.xpath(".//*[@id='ContentPlaceHolder1_ddSignedCP']"))).selectByIndex(1);
+		Thread.sleep(1000);
+		new Select(driver.findElement(By.xpath(".//*[@id='ContentPlaceHolder1_ddSignedCE']"))).selectByIndex(1);
+		Thread.sleep(1000);
+		new Select(driver.findElement(By.xpath(".//*[@id='ContentPlaceHolder1_ddStrength']"))).selectByIndex(1);
+		Thread.sleep(1000);
+		new Select(driver.findElement(By.xpath(".//*[@id='ContentPlaceHolder1_ddPartyA']"))).selectByIndex(1);
+		Thread.sleep(1000);
+		new Select(driver.findElement(By.xpath(".//*[@id='ContentPlaceHolder1_ddPartyB']"))).selectByIndex(2);
+		Thread.sleep(1000);
+		new Select(driver.findElement(By.xpath(".//*[@id='ContentPlaceHolder1_ddAgreementslist']"))).selectByIndex(1);
+		Thread.sleep(1000);
+		driver.findElement(By.xpath(".//*[@id='ContentPlaceHolder1_btnSubmit']")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath(".//*[@id='ContentPlaceHolder1_lblStatus']/a")).click();
+		Thread.sleep(4000);
+		driver.findElement(By.xpath(".//*[@id='ContentPlaceHolder1_btnEdit']")).click();
+		Thread.sleep(3000);
+		new Select(driver.findElement(By.xpath(".//*[@id='ContentPlaceHolder1_ddlCounterParty']"))).selectByIndex(15);
+		Thread.sleep(3000);
+		
+		
+		new Select(driver.findElement(By.xpath(".//*[@id='ContentPlaceHolder1_ddl_mm']"))).selectByIndex(5);
+		new Select(driver.findElement(By.xpath(".//*[@id='ContentPlaceHolder1_ddl_dd']"))).selectByIndex(5);
+		new Select(driver.findElement(By.xpath(".//*[@id='ContentPlaceHolder1_ddl_yyyy']"))).selectByIndex(5);
+		
+		new Select(driver.findElement(By.xpath(".//*[@id='ContentPlaceHolder1_ddSignedCP']"))).selectByIndex(2);
+		Thread.sleep(1000);
+		new Select(driver.findElement(By.xpath(".//*[@id='ContentPlaceHolder1_ddSignedCE']"))).selectByIndex(2);
+		Thread.sleep(1000);
+		
+		new Select(driver.findElement(By.xpath(".//*[@id='ContentPlaceHolder1_ddPartyA']"))).selectByIndex(2);
+		Thread.sleep(1000);
+		new Select(driver.findElement(By.xpath(".//*[@id='ContentPlaceHolder1_ddPartyB']"))).selectByIndex(1);
+		Thread.sleep(1000);
+		
+		driver.findElement(By.xpath(".//*[@id='ContentPlaceHolder1_btnSubmit']")).click();
+		Thread.sleep(3000);
+		
+		
+		
+		
+		
+		
+		
 		
 		//driver.close();
 		
